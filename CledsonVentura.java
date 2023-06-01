9f722ab0383c4bd19f225bc07042611c86d44bcf
Gabriel Leandro Pontes da Silva <gabriel.pontes@labs.dcx>
2023-06-01 11:22:11 -0300

    add file java

---
+++ CledsonVentura.java
@@ -0,0 +1,6 @@
+public class CledsonVentura{
+	public static void main(String []args) {
+		System.out.println("Ola Analia");
+		
+	}
+}
---
+++ GabrielLeandro.java
@@ -0,0 +1,11 @@
+package repositorioAula;
+
+public class GabrielLeandro {
+	public static void main(String []args) {
+		int numA = 5;
+		int numB = 10;
+		int soma = numA+numB;
+		System.out.println("Resultado:"+soma);
+	}
+
+}
